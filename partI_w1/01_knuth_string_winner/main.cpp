3207c889dc2807fa636da4d6a7f7726952974951
waterinocean <ajiql@126.com>
2021-10-22 18:32:24 +0800

    init comit

---
+++ partI_w1/01_knuth_string_winner/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <random>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** args) {
+    //first
+    int i = 0;
+    while (args[i] != NULL)
+    {
+        string winner = args[i];
+        if (bernoulli_distribution)
+
+        i++;
+    }
+
+    //second
+    
+
+    return 0;
+}
